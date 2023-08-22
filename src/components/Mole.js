6f1b746fbf19c0fc3df1243295b982938af35f43
Aaron Nash <aaron27nash@gmail.com>
2023-08-21 22:20:17 -0400

    finished

---
+++ src/components/Mole.js
@@ -0,0 +1,22 @@
+import { useEffect } from "react"
+import moleImg from '../mole.png'
+
+function Mole(props) {
+  useEffect(() => {
+    let randomSeconds = Math.ceil(Math.random() * 5000)
+        let timer = setTimeout(() => {
+            props.toggle(false)
+        }, randomSeconds)
+        return () => clearTimeout(timer)
+  })
+
+  return (
+    <div>
+      <img style={{'width': '30vw'}} src={moleImg} onClick={props.handleBopping} />
+    </div>
+  )
+}
+
+export default Mole
+
+
